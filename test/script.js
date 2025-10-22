74e891e5f01e36cb8b4817f29bb02ea593c4f2f1
Melissa <melissa.ihiga@gmail.com>
2025-10-22 15:21:51 +0300

    module 5 prep

---
+++ test/script.js
@@ -0,0 +1,8 @@
+function checkPassOrFail(marks){
+        if(marks >= 40){
+            return "pass";
+        } else {
+            return "fail";
+        }
+    }
+console.log(checkPassOrFail(50)); // pass

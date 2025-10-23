b35f8f2deb12ff6e33998293bfce07fe670020ab
Melissa <melissa.ihiga@gmail.com>
2025-10-23 18:35:57 +0300

    passing buildAndShowHomeHTML as a value to be called

--- module5/js/script.js
+++ module5/js/script.js
@@ -83,7 +83,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [homeHtmlUrl], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  // Without parenthesis passes the function as value to be called later when we get
+  // the server response.
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -121,7 +123,7 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       insertHtml("#main-content", homeHtml);
-      
+
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
